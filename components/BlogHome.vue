b38175403dd2a181892da40b54f72061d4c24cd6
creeea <gilles@tezla.ch>
2023-01-03 16:36:43 +0100

    functional dynamic changes in css for coins

--- components/BlogHome.vue
+++ components/BlogHome.vue
@@ -5,8 +5,8 @@
                 <div class="block-title__text"><span>Evolutionary, revolutionary, and</span> <br> <span>everything in-between.</span></div><!-- /.block-title__text -->
             </div><!-- /.block-title -->
             <div class="row">
-
-                <div class="col-lg-4"><!-- Cosmos -->
+                
+                <div class="col-lg-4 cosmos"><!-- Cosmos -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 May, 2019</div><!-- /.blog-one__date -->
@@ -14,7 +14,7 @@
                             <img src="/images/cosmos.png" alt="">
                             </div>
                             <div>
-                            <h3 class="blog-one__title"><nuxt-link to="/blog-details">Cosmos</nuxt-link></h3>
+                            <h3 class="blog-one__title">Cosmos</h3>
                             <p class="blog-one__text">The Cosmos Network is a decentralized network and the economic center of independent, scalable, and interoperable (IBC) blockchains.</p>
                             </div>
                             <div class="blog__footer"><a class="blog-one__dev">Mainnet, 1% fee</a>
@@ -25,7 +25,8 @@
                     </div><!-- /.blog-one__single -->
                 </div><!-- /.col-lg-4 -->
 
-                <div class="col-lg-4"><!-- Nym-->
+
+                <div class="col-lg-4 nym"><!-- Nym-->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">15 November, 2021</div><!-- /.blog-one__date -->
@@ -45,7 +46,87 @@
                     </div><!-- /.blog-one__single -->
                 </div><!-- /.col-lg-4 -->
 
-                  <div class="col-lg-4"><!-- Evmos-->
+                <div class="col-lg-4 azero"><!-- AZERO-->
+                    <div class="blog-one__single">
+                        <div class="blog-one__single-inner-block">
+                            <div class="blog-one__date">29 December, 2022</div><!-- /.blog-one__date -->
+                            <div class="blog-one__image">
+                            <img src="/images/azero.png" alt="">
+                            </div>
+                            <div>
+                            <h3 class="blog-one__title">Aleph Zero</h3>
+                            <p class="blog-one__text">Aleph Zero is a layer 1 privacy blockchain that ensures scalability, low transaction fees, and maximum security guarantees for developers.</p>
+                            </div>
+                            <div class="blog__footer"><a class="blog-one__dev">Mainnet, 2% fee</a>
+                            <a target="_blank" href ="https://alephzero.subscan.io/waiting/5FTsYv3dgJsjndE3ktF1Rp3rF6UjGybGW2mcYVuaFzfrC4Cv" class="blog-one__btn">Explorer</a>                           
+                           
+                            </div>
+                                     
+                        </div><!-- /.blog-one__single-inner-block -->
+                    </div><!-- /.blog-one__single -->
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col-lg-4 ethereum"><!-- ETH 2.0 -->
+                    <div class="blog-one__single">
+                        <div class="blog-one__single-inner-block">
+                            <div class="blog-one__date">10 June, 2021</div><!-- /.blog-one__date -->
+                            <div class="blog-one__image">
+                            <img src="/images/ethereum.png" alt="">
+                            </div>
+                            <div>
+                            <h3 class="blog-one__title">Ethereum</h3>
+                            <p class="blog-one__text">Ethereum switched to proof-of-stake in 2022 because it is more secure, less energy-intensive, and better for implementing new scaling solutions.</p>
+                            </div>
+                            <div class="blog__footer"><a class="blog-one__dev">Mainnet</a>
+                            <a  target="_blank" class="blog-one__btn" v-on:click="eth_alert">Info</a>                             
+                            </div>
+                                    
+                        </div><!-- /.blog-one__single-inner-block -->
+                    </div><!-- /.blog-one__single -->
+                </div><!-- /.col-lg-4 -->
+
+
+                <div class="col-lg-4 obol"><!-- ETH 2.0 -->
+                    <div class="blog-one__single">
+                        <div class="blog-one__single-inner-block">
+                            <div class="blog-one__date">1 November, 2022</div><!-- /.blog-one__date -->
+                            <div class="blog-one__image">
+                            <img src="/images/obol.png" alt="">
+                            </div>
+                            <div>
+                            <h3 class="blog-one__title">Obol</h3>
+                            <p class="blog-one__text">The Obol Network is an ecosystem for trust minimized staking that enables people to create, test, run & co-ordinate distributed validators.</p>
+                            </div>
+                            <div class="blog__footer"><a class="blog-one__dev">Testnet</a>
+                            <a  target="_blank" class="blog-one__btn" v-on:click="obol_alert">Info</a>                             
+                            </div>
+                                    
+                        </div><!-- /.blog-one__single-inner-block -->
+                    </div><!-- /.blog-one__single -->
+                </div><!-- /.col-lg-4 -->
+
+
+                <div class="col-lg-4 osmosis"><!-- Osmosis -->
+                    <div class="blog-one__single">
+                        <div class="blog-one__single-inner-block">
+                            <div class="blog-one__date">1 July, 2021</div><!-- /.blog-one__date -->
+                            <div class="blog-one__image">
+                            <img src="/images/osmosis.png" alt="">
+                            </div>
+                            <div>
+                            <h3 class="blog-one__title">Osmosis</h3>
+                            <p class="blog-one__text">Osmosis is an advanced AMM protocol built using the Cosmos SDK that will allow developers to design, build, and deploy their own customized AMMs.</p>
+                            </div>
+                            <div class="blog__footer"><a class="blog-one__dev">Mainnet, 5% fee</a>
+                            <a  class="blog-one__btn" href="https://ping.pub/osmosis/staking/osmovaloper1y0us8xvsvfvqkk9c6nt5cfyu5au5tww24nrlnx" target="blank" >Delegate</a>                                                 
+                            </div>
+                            
+                        </div><!-- /.blog-one__single-inner-block -->
+                    </div><!-- /.blog-one__single -->
+                </div><!-- /.col-lg-4 -->
+
+                
+                <div class="col-lg-4 evmos"><!-- Evmos-->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 November, 2021</div><!-- /.blog-one__date -->
@@ -64,28 +145,11 @@
                         </div><!-- /.blog-one__single-inner-block -->
                     </div><!-- /.blog-one__single -->
                 </div><!-- /.col-lg-4 -->
-                
-                                
-                <div class="col-lg-4"><!-- Osmosis -->
-                    <div class="blog-one__single">
-                        <div class="blog-one__single-inner-block">
-                            <div class="blog-one__date">1 July, 2021</div><!-- /.blog-one__date -->
-                            <div class="blog-one__image">
-                            <img src="/images/osmosis.png" alt="">
-                            </div>
-                            <div>
-                            <h3 class="blog-one__title"><nuxt-link to="/blog-details">Osmosis</nuxt-link></h3>
-                            <p class="blog-one__text">Osmosis is an advanced AMM protocol built using the Cosmos SDK that will allow developers to design, build, and deploy their own customized AMMs.</p>
-                            </div>
-                            <div class="blog__footer"><a class="blog-one__dev">Mainnet, 5% fee</a>
-                            <a  class="blog-one__btn" href="https://ping.pub/osmosis/staking/osmovaloper1y0us8xvsvfvqkk9c6nt5cfyu5au5tww24nrlnx" target="blank" >Delegate</a>                                                 
-                            </div>
-                            
-                        </div><!-- /.blog-one__single-inner-block -->
-                    </div><!-- /.blog-one__single -->
-                </div><!-- /.col-lg-4 -->
 
-                <div class="col-lg-4"><!-- Juno -->
+                                
+
+
+                <div class="col-lg-4 juno"><!-- Juno -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 October, 2021</div><!-- /.blog-one__date -->
@@ -93,7 +157,7 @@
                             <img src="/images/juno.png" alt="">
                             </div>
                             <div>
-                            <h3 class="blog-one__title"><nuxt-link to="/blog-details">Juno</nuxt-link></h3>
+                            <h3 class="blog-one__title">Juno</h3>
                             <p class="blog-one__text">Juno is an interoperable smart contract network. Highly scalable, robust, secure and easy to deploy - built on the Cosmos SDK.</p>
                             </div>
                             <div class="blog__footer"><a class="blog-one__dev">Mainnet, 5% fee</a>
@@ -105,7 +169,7 @@
                 </div><!-- /.col-lg-4 -->
 
 
-                <div class="col-lg-4"><!-- Chainlink -->
+                <div class="col-lg-4 chainlink"><!-- Chainlink -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 July, 2021</div><!-- /.blog-one__date -->
@@ -113,7 +177,7 @@
                             <img src="/images/chainlink.png" alt="">
                             </div>
                             <div>
-                            <h3 class="blog-one__title"><nuxt-link to="/blog-details">Chainlink</nuxt-link></h3>
+                            <h3 class="blog-one__title">Chainlink</h3>
                             <p class="blog-one__text">Chainlink is a decentralized network that provides data and information from off-blockchain sources to on-blockchain smart contracts via oracles.</p>
                             </div>
                             <div class="blog__footer"><a class="blog-one__dev">Mainnet</a>
@@ -124,27 +188,10 @@
                 </div><!-- /.col-lg-4 -->
 
 
-                <div class="col-lg-4"><!-- ETH 2.0 -->
-                    <div class="blog-one__single">
-                        <div class="blog-one__single-inner-block">
-                            <div class="blog-one__date">10 June, 2021</div><!-- /.blog-one__date -->
-                            <div class="blog-one__image">
-                            <img src="/images/ethereum.png" alt="">
-                            </div>
-                            <div>
-                            <h3 class="blog-one__title">ETH 2.0</h3>
-                            <p class="blog-one__text">ETH 2.0 is an upgrade to the Ethereum network that aims to improve the network's security and scalability - shifting their current mining model to a staking model.</p>
-                            </div>
-                            <div class="blog__footer"><a class="blog-one__dev">Mainnet</a>
-                            <a  target="_blank" class="blog-one__btn" v-on:click="eth_alert">Info</a>                             
-                            </div>
-                                    
-                        </div><!-- /.blog-one__single-inner-block -->
-                    </div><!-- /.blog-one__single -->
-                </div><!-- /.col-lg-4 -->
+                
 
                 
-                <div class="col-lg-4"><!-- Umee -->
+                <div class="col-lg-4 umee"><!-- Umee -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">21. December, 2021</div><!-- /.blog-one__date -->
@@ -153,7 +200,7 @@
                             </div>
                             <div>
                             <h3 class="blog-one__title">Umee</h3>
-                            <p class="blog-one__text">Umee connects users to create lending and borrowing positions, move capital across chains, discover new yield opportunities and explore DeFi applications intersecting networks.</p>
+                            <p class="blog-one__text">Umee connects users to create lending and borrowing positions, move capital across chains, discover new yield opportunities and explore DeFi applications.</p>
                             </div>
                             <div><a class="blog-one__dev">Mainnet, 2% fee</a>
                             <a href="https://wallet.keplr.app/#/umee/stake?modal=stake&validator=umeevaloper19yy0u0ttam2p9k874vsh2u4tt7e6cmq8h2w9nv" target="_blank" class="blog-one__btn">Delegate</a>                     
@@ -165,7 +212,7 @@
                 </div><!-- /.col-lg-4 -->
 
 
-                <div class="col-lg-4"><!-- Tezos -->
+                <div class="col-lg-4 tezos"><!-- Tezos -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">15 July, 2018</div><!-- /.blog-one__date -->
@@ -173,10 +220,10 @@
                             <img src="/images/tezos.png" alt="">
                             </div>
                             <div>
-                            <h3 class="blog-one__title"><nuxt-link to="/blog-details">Tezos</nuxt-link></h3>
-                            <p class="blog-one__text">Tezos is a platform that addresses key barriers facing blockchain adoption for assets and applications backed by a global community of validators, researchers, and builders.</p>
+                            <h3 class="blog-one__title">Tezos</h3>
+                            <p class="blog-one__text">Tezos is designed to power the Web3 revolution - a blockchain designed to evolve. Security focused. Upgradeable. Built to last.</p>
                             </div>
-                            <div class="blog__footer"><a class="blog-one__dev">Mainnet, 10% fee</a>
+                            <div> <a class="blog-one__dev">Mainnet, 10% fee</a>
                             <a  class="blog-one__btn" href="https://tzstats.com/tz1hAYfexyzPGG6RhZZMpDvAHifubsbb6kgn" target="blank" >Delegate</a>                                                 
                             </div>
                            
@@ -188,7 +235,7 @@
 
                 
 
-                <div class="col-lg-4"><!-- Sentinel-->
+                <div class="col-lg-4 sentinel"><!-- Sentinel-->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 June, 2021</div><!-- /.blog-one__date -->
@@ -207,7 +254,7 @@
                     </div><!-- /.blog-one__single -->
                 </div><!-- /.col-lg-4 -->
 
-                <div class="col-lg-4"><!-- Anoma -->
+                <div class="col-lg-4 anoma"><!-- Anoma -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 November, 2021</div><!-- /.blog-one__date -->
@@ -228,7 +275,7 @@
                 </div><!-- /.col-lg-4 -->
 
                   
-                <div class="col-lg-4"><!-- Avalanche -->
+                <div class="col-lg-4 avalanche"><!-- Avalanche -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">1 October, 2020</div><!-- /.blog-one__date -->
@@ -236,7 +283,7 @@
                             <img src="/images/avalanche.png" alt="">
                             </div>
                             <div>
-                            <h3 class="blog-one__title"><nuxt-link to="/blog-details">Avalanche</nuxt-link></h3>
+                            <h3 class="blog-one__title">Avalanche</h3>
                             <p class="blog-one__text">Avalanche is the fastest smart contracts platform in the blockchain industry, as measured by time-to-finality with EVM compatibility.</p>
                             </div>
                             <div class="blog__footer"><a class="blog-one__dev">Mainnet, 10% fee</a>
@@ -247,7 +294,7 @@
                 </div><!-- /.col-lg-4 -->
  
 
-                <div class="col-lg-4"><!-- Gravity -->
+                <div class="col-lg-4 gravity"><!-- Gravity -->
                     <div class="blog-one__single">
                         <div class="blog-one__single-inner-block">
                             <div class="blog-one__date">17. December, 2021</div><!-- /.blog-one__date -->
@@ -292,7 +339,9 @@
             },
             link_alert: function test(){
                 this.$alert("These nodes are ran by us for an institutional client. Additionally, Chainlink does not support staking yet. If you are interested in any collaboration regarding Chainlink, please contact us.");
-                
+        },
+            obol_alert: function test(){
+                this.$alert("We are actively running a DVT cluster of 4 with treshold 3. If you are interested to create a DVT cluster with us on mainnet please reach out to us.");
         }
         
     }
